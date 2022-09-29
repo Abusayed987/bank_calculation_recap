c741651caf622dd6dff91883f8ce074dfd68ef1c
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-29 07:04:31 +0600

    Money withdraw and reduce balance for withdraw

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,4 @@
+/**
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositField = document.getElementById('deposit_field');
     const newDepositFieldString = depositField.value ;
@@ -21,5 +22,55 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const currentTotalBlance = newTotalBlanceNumber + newDepositFieldNumber  ;
     totalBlance.innerText = currentTotalBlance;
 
+
+})
+Recap Now--------------------------!!!
+
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    const depositFeild = document.getElementById('deposit_field');
+    const newDepositFieldString = depositFeild.value;
+    const newDepositFieldNumber = parseFloat(newDepositFieldString);
+    depositFeild.value = '';
+
+    if(isNaN(newDepositFieldNumber)){
+        alert('Please type a Correct Number')
+        return;
+    }
+
+    const totalDeposit = document.getElementById('deposit_total');
+    const totalDepositString = totalDeposit.innerText;
+    const totalDepositNumber = parseFloat(totalDepositString);
+    totalDeposit.innerText =  totalDepositNumber + newDepositFieldNumber ;
+
+    const totalBlance = document.getElementById('blance_total');
+    const totalBlanceString = totalBlance.innerText;
+    const totalBlanceNumber = parseFloat(totalBlanceString);
+    
+    const currentTotalBlance =totalBlanceNumber + newDepositFieldNumber;
+    totalBlance.innerText = currentTotalBlance;
+})
+Recap Now--------------------------!!!
+ **/
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    const inputField = document.getElementById('deposit_field');
+    const newInputFieldValueStyring = inputField.value
+    const newInputFieldValueNumber = parseFloat(newInputFieldValueStyring);
+    inputField.value = '';
+
+    if (isNaN(newInputFieldValueNumber)) {
+        alert('Please Provide a Valid Number');
+        return;
+    }
+
+    const depositTotalElement = document.getElementById('deposit_total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotalNumber = parseFloat(previousDepositTotalString);
+    depositTotalElement.innerText = previousDepositTotalNumber + newInputFieldValueNumber;
+
+    const blanceTotalElement = document.getElementById('blance_total')
+    const previousBlanceTotalVlueString = blanceTotalElement.innerText;
+    const previousBlanceTotalVlueNumber = parseFloat(previousBlanceTotalVlueString) ;
+
+    blanceTotalElement.innerText = previousBlanceTotalVlueNumber + newInputFieldValueNumber ;
 
 })
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,25 +1,76 @@
+/**
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw_field');
     const newWihdrawFieldString = withdrawField.value ;
-    const newDepositFieldNumber = parseFloat(newWihdrawFieldString);
+    const newwithdrawFieldNumber = parseFloat(newWihdrawFieldString);
     withdrawField.value = '';
 
-    if (isNaN(newDepositFieldNumber)) {
+    if (isNaN(newwithdrawFieldNumber)) {
         alert('please type a valid Number')
         return;
     }
-    const depositTotalElement = document.getElementById('withdraw_total');
-    const previousDepositTotalString = depositTotalElement.innerText;
-    const previousDepositTotalNumber = parseFloat(previousDepositTotalString);   
-    const newDepositTotal = newDepositFieldNumber + previousDepositTotalNumber;
-    depositTotalElement.innerText = newDepositTotal ;
+    const withdrawTotalElement = document.getElementById('withdraw_total');
+    const previouswithdrawTotalString = withdrawTotalElement.innerText;
+    const previouswithdrawTotalNumber = parseFloat(previouswithdrawTotalString);   
+    const newwithdrawTotal = newwithdrawFieldNumber + previouswithdrawTotalNumber;
+    withdrawTotalElement.innerText = newwithdrawTotal ;
 
     const totalBlance = document.getElementById('blance_total');
     const totalBlanceString = totalBlance.innerText;
     const newTotalBlanceNumber = parseFloat(totalBlanceString)
 
-    const currentTotalBlance = newTotalBlanceNumber - newDepositFieldNumber  ;
+    const currentTotalBlance = newTotalBlanceNumber - newwithdrawFieldNumber  ;
     totalBlance.innerText = currentTotalBlance;
 
+
+})
+Recap Now-----------------------------!!!
+
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const withdrawFeild = document.getElementById('withdraw_field');
+    const newWithdrawFieldString = withdrawFeild.value;
+    const newWithdrawFieldNumber = parseFloat(newWithdrawFieldString);
+    withdrawFeild.value = '';
+
+    if(isNaN(newWithdrawFieldNumber)){
+        alert('Please type a Correct Number')
+        return;
+    }
+
+    const totalWithdraw = document.getElementById('withdraw_total');
+    const totalWithdrawString = totalWithdraw.innerText;
+    const totalWithdrawNumber = parseFloat(totalWithdrawString);
+    totalWithdraw.innerText =  totalWithdrawNumber + newWithdrawFieldNumber ;
+
+    const totalBlance = document.getElementById('blance_total');
+    const totalBlanceString = totalBlance.innerText;
+    const totalBlanceNumber = parseFloat(totalBlanceString);
+    
+    const currentTotalBlance =totalBlanceNumber - newWithdrawFieldNumber;
+    totalBlance.innerText = currentTotalBlance;
+})
+Recap Now-----------------------------!!!
+ */
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const inputField = document.getElementById('withdraw_field');
+    const newInputFieldValueStyring = inputField.value
+    const newInputFieldValueNumber = parseFloat(newInputFieldValueStyring);
+    inputField.value = '';
+
+    if (isNaN(newInputFieldValueNumber)) {
+        alert('Please Provide a Valid Number');
+        return;
+    }
+
+    const withdrawTotalElement = document.getElementById('withdraw_total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotalNumber = parseFloat(previousWithdrawTotalString);
+    withdrawTotalElement.innerText = previousWithdrawTotalNumber + newInputFieldValueNumber;
+
+    const blanceTotalElement = document.getElementById('blance_total')
+    const previousBlanceTotalVlueString = blanceTotalElement.innerText;
+    const previousBlanceTotalVlueNumber = parseFloat(previousBlanceTotalVlueString) ;
+
+    blanceTotalElement.innerText = previousBlanceTotalVlueNumber - newInputFieldValueNumber ;
 
 })
